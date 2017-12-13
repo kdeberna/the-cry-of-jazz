1672c54398423388fb5df3bdf7eda6755ba2761f
Kevin DeBernardi <kevin@analoglifestyle.com>
2017-12-13 16:43:10 -0500

    Add Film view and route

---
+++ resources/views/film.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Film')
+
+@section('content')
+    <h1>The Film</h1>
+@endsection
+
+@section('scripts')
+    @parent
+    <p>more scripts</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/film', function () {
+    return view('film');
+});
