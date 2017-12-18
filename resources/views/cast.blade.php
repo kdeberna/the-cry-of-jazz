f495f21c9ffb75692ed8c3f08c87d762217a8bd3
Kevin DeBernardi <kevin@analoglifestyle.com>
2017-12-18 14:42:45 -0500

    Add Cast & Crew content

--- resources/views/cast.blade.php
+++ resources/views/cast.blade.php
@@ -1,9 +1,166 @@
 @extends('layouts.app')
 
-@section('title', 'Cast & Crew')
+@section('title', 'Cast &amp; Crew')
 
 @section('content')
     <h1>Cast &amp; Crew</h1>
+    <p><i>The Cry of Jazz</i> wouldn't have been possible without every participant volunteering their time. Approximately 65 people&mdash;actors, musicians, crew&mdash;worked for free. Experience and exposure is what everyone was offered instead of a wage. The actors consisted of both students and local amateurs from Chicago's black community theater scene. The crew was made up of friends and aquaintances who assisted in everything from driving actors and crew around town to providing the apartment set and props used in the film.</p>
+    <p>The musicians performing in the film in various guises are Le Sun Ra &amp; his Arkestra, featuring the likes of John Gilmour and Julian Priester. Paid a nominal fee, Sun Ra was looking to promote music from his own El Saturn Records label used in the film, as well as hoped it would garner future live gigs.</p>
+    <p>Two cinematographers shot the film: Hank Starr and Howard Alk. Alk&mdash;who Ed Bland knew from the Gate of Horn music club where the Cry's music scenes were shot&mdash;was also a film editor, and was hired to edit <i>The Cry</i>. He would later go on to make a number of acclaimed documentary films on Fred Hampton and Bob Dylan.</p>
+	<div class="credits">
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Director</span>
+			<span class="credit"><a href="http://www.edblandmusic.com/" target="_blank">Edward O. Bland</a></span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Editor</span>
+			<span class="credit">Howard Alk</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Writer</span>
+			<span class="credit">Edward O. Bland</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Writer</span>
+			<span class="credit">Nelam L. Hill</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Writer</span>
+			<span class="credit">Mark Kennedy</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Producer</span>
+			<span class="credit">Edward O. Bland</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Producer</span>
+			<span class="credit">Nelam L. Hill</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Alex</span>
+			<span class="credit">George Waller</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Natalie</span>
+			<span class="credit">Dorothea Horton</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Faye</span>
+			<span class="credit">Linda Dillon</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">John</span>
+			<span class="credit">Andrew Duncan</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Louis</span>
+			<span class="credit">Laroy Inman</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Bob</span>
+			<span class="credit">James Miller</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Bruce</span>
+			<span class="credit">Gavin McFadyen</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">???</span>
+			<span class="credit">Carl Dupree</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">???</span>
+			<span class="credit">Goldina Rojas</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">???</span>
+			<span class="credit">Alan Leavitt</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">???</span>
+			<span class="credit">Carl McCormack</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">???</span>
+			<span class="credit">Lettie Mae Randolph</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Director of Photography</span>
+			<span class="credit">Hank Starr</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Additional Photograpahy</span>
+			<span class="credit">Jonathan Chernoble</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Lighting</span>
+			<span class="credit">Valentin Scheglowski</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Stills</span>
+			<span class="credit">Valentin Scheglowski</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Stills</span>
+			<span class="credit">Betty McCarthy</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Assistant Director</span>
+			<span class="credit">Nelam L. Hill</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Art Director</span>
+			<span class="credit">Bernard Goss</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Script Consultant</span>
+			<span class="credit">Eugene Titus</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Script Consultant</span>
+			<span class="credit">Madeline Tourtelot</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Script Consultant</span>
+			<span class="credit">Frank McGovern</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Sound Crew</span>
+			<span class="credit">Garry Harris</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Sound Crew</span>
+			<span class="credit">Timmy Harris</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Sound Crew</span>
+			<span class="credit">Nelam L. Hill</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Music Written By</span>
+			<span class="credit">Le Sun Ra</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Music Written By</span>
+			<span class="credit">Paul Severson</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Music Written By</span>
+			<span class="credit">Julian Priester</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Music Written By</span>
+			<span class="credit">Eddie Higgins</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #111111"><span class="role">Music Written By</span>
+			<span class="credit">Norman Leist</span></p>
+		</div>
+		<div class="entry">
+			<p style="background-color: #0D0D0D"><span class="role">Musical Director</span>
+			<span class="credit">Edward O. Bland</span></p>
+		</div>
+	</div>
 @endsection
 
 @section('scripts')
