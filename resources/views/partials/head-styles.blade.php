b3b480f1b619d5f5e5705be932d69992e502f95d
Kevin DeBernardi <kevin@analoglifestyle.com>
2017-12-29 14:18:31 -0500

    Move the critical render styles into a partial for inclusion in the head block

---
+++ resources/views/partials/head-styles.blade.php
@@ -0,0 +1,25 @@
+<style>
+    .embed-container {
+        position: relative;
+        padding-bottom: 56.25%;
+        overflow: hidden;
+    }
+    .embed-container iframe,
+    .embed-container object,
+    .embed-container embed {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+    .navbar-default .navbar-toggle:hover {
+        background-color: transparent;
+    }
+    .navbar-collapse {
+        box-shadow: none;
+    }
+    .navbar-nav > li > a {
+        padding-left: 0;
+    }
+</style>
