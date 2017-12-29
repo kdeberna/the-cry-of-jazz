a9083fd05b6bced59491c3462b8f67d765d91e35
Kevin DeBernardi <kevin@analoglifestyle.com>
2017-12-29 14:24:56 -0500

    Add and style the elements for the Contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,42 +4,38 @@
 
 @section('content')
     <h1>Contact Us</h1>
+    <p>Questions about <i>The Cry of Jazz</i>? Are you interested in <a href="">screening</a> the film at an event or festival? Contact us via the form below and we will respond as soon as we can.</p>
 
-	<div class="container">	
-		@if(session('message'))
+	@if(session('message'))
 		<div class='alert alert-success'>
 			{{ session('message') }}
 		</div>
-		@endif
-		
-		<div class="col-12 col-md-6">
-			<form class="form-horizontal" method="POST" action="/contact">
-				{{ csrf_field() }} 
-				<div class="form-group">
-				<label for="Name">Name: </label>
+	@endif
+
+	<div class="col-12 col-md-6">
+		<form class="form-horizontal" method="POST" action="/contact">
+			{{ csrf_field() }} 
+			<div class="form-group">
+				<label for="Name">Name</label>
 				<input type="text" class="form-control" id="name" placeholder="Your name" name="name" required>
 			</div>
-
 			<div class="form-group">
-				<label for="email">Email: </label>
+				<label for="email">Email</label>
 				<input type="text" class="form-control" id="email" placeholder="john@example.com" name="email" required>
 			</div>
-
 			<div class="form-group">
-				<label for="message">message: </label>
-				<textarea type="text" class="form-control luna-message" id="message" placeholder="Type your messages here" name="message" required></textarea>
+				<label for="message">Message</label>
+				<textarea type="text" class="form-control luna-message" id="message" placeholder="Type your message here" name="message" style="height: 200px;" required></textarea>
 			</div>
-
-				<div class="form-group">
-					<button type="submit" class="btn btn-primary" value="Send">Send</button>
-				</div>
-			</form>
-		</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-default" value="Send" style="width: 100%;">Send your message</button>
+			</div>
+		</form>
 	</div>
 
 @endsection
 
 @section('scripts')
     @parent
-    <p>more scripts</p>
+    <!-- <script type="text/javascript" src="/js/app.js"></script> -->
 @endsection
