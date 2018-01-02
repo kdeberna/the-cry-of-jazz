362230b76a778e222a9b1bc40ce0d582a3b8953e
Kevin DeBernardi <kevin@analoglifestyle.com>
2018-01-02 13:40:07 -0500

    Add content to Privacy Policy page

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -4,6 +4,63 @@
 
 @section('content')
     <h1>Privacy Policy</h1>
+    <p>Last Updated: January 01, 2018</p>
+	<p>Music Media Group, Inc. respects the privacy of our users and has developed this Privacy Policy to demonstrate its commitment to protecting your privacy. These privacy policies (the "Privacy Policy") are intended to describe for you, as an individual who is a user of thecryofjazz.com, the information we collect, how that information may be used, with whom it may be shared, and your choices about such uses and disclosures.</p>
+	<p>We encourage you to read this Privacy Policy carefully when using our website or services or transacting business with us. By using our website, you are accepting the practices described in this Privacy Policy.</p>
+	<p>If you have any questions about our privacy practices, please refer to the end of this Privacy Policy for information on how to contact us.</p>
+
+	<h2>What We Do With Your Information?</h2>
+	<p>When you browse our website, we automatically receive your computer's internet protocol (IP) address in order to provide us with information that helps us learn about your browser and operating system.</p>
+	<p>Email marketing (if applicable): With your permission, we may send you emails about our store, new products, and other updates.</p>
+
+	<h2>Consent</h2>
+	<p><strong>How do you get my consent?</strong><br/>When you provide us with personal information to complete a transaction, verify your credit card, place an order, arrange for a delivery or return a purchase, we imply that you consent to our collecting it and using it for that specific reason only.</p>
+	<p>If we ask for your personal information for a secondary reason, like marketing, we will either ask you directly for your expressed consent, or provide you with an opportunity to say no.</p>
+	<p><strong>How do I withdraw my consent?</strong><br />If after you opt-in, you change your mind, you may withdraw your consent for us to contact you, for the continued collection, use or disclosure of your information, at anytime, by contacting us at k.debernardi@musicmediagroup.net or mailing us at: Music Media Group, Inc. 475 Evergreen Avenue, Brooklyn, NY, 11221, United States
+
+	<h2>Disclosure</h2>
+	<p>We may disclose your personal information if we are required by law to do so or if you violate our Terms of Service.</p>
+
+	<h2>Third-Party Services</h2>
+	<p>In general, the third-party providers used by us will only collect, use and disclose your information to the extent necessary to allow them to perform the services they provide to us.</p>
+	<p>However, certain third-party service providers, such as payment gateways and other payment transaction processors, have their own privacy policies in respect to the information we are required to provide to them for your purchase-related transactions.</p>
+	<p>For these providers, we recommend that you read their privacy policies so you can understand the manner in which your personal information will be handled by these providers.</p>
+	<p>In particular, remember that certain providers may be located in or have facilities that are located in a different jurisdiction than either you or us. So if you elect to proceed with a transaction that involves the services of a third-party service provider, then your information may become subject to the laws of the jurisdiction(s) in which that service provider or its facilities are located.</p>
+	<p>As an example, if you are located in Canada and your transaction is processed by a payment gateway located in the United States, then your personal information used in completing that transaction may be subject to disclosure under United States legislation, including the Patriot Act.</p>
+	<p>Once you leave our store’s website or are redirected to a third-party website or application, you are no longer governed by this Privacy Policy or our website’s Terms of Service.</p>
+
+	<h2>Links</h2>
+	<p>When you click on links on our store, they may direct you away from our site. We are not responsible for the privacy practices of other sites and encourage you to read their privacy statements.</p>
+
+	<h2>Security</h2>
+	<p>To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.</p>
+	<p>If you provide us with your credit card information, the information is encrypted using secure socket layer technology (SSL) and stored with a AES-256 encryption.  Although no method of transmission over the Internet or electronic storage is 100% secure, we follow all PCI-DSS requirements and implement additional generally accepted industry standards.</p>
+
+	<h2>Cookies</h2>
+	<p>Here is a list of cookies that we use. We’ve listed them here so you can choose if you want to opt-out of cookies or not.</p>
+
+	<p>
+		laravel_session, unique token, sessional, used to identify a session instance for a user.<br />
+		XSRF-TOKEN, unique token, used to prevent cross-site scripting attacks
+	</p>
+
+	<h2>Age of Consent</h2>
+	<p>By using this site, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.</p>
+
+	<h2>Changes to this Privacy Policy</h2>
+	<p>We reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and clarifications will take effect immediately upon their posting on the website. If we make material changes to this policy, we will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it.</p>
+
+	<h2>Questions &amp; Contact Information</h2>
+	<p>If you would like to: access, correct, amend or delete any personal information we have about you, register a complaint, or simply want more information contact our Privacy Compliance Officer at k.debernardi@musicmediagroup.net or by mail at Music Media Group, Inc.</p>
+
+	<p>
+		Music Media Group, Inc.<br />
+		Re: Privacy Compliance Officer<br />
+		475 Evergreen Avenue<br />
+		Brooklyn, NY, 11221<br />
+		United States
+	</p>
+
 @endsection
 
 @section('scripts')
