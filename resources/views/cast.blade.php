16ac13956fac49aad053446e565eac21b4c28319
Kevin DeBernardi <kevin@analoglifestyle.com>
2018-01-03 18:56:18 -0500

    Move heading out of the column layout and fix resulting styling issues for desktop

--- resources/views/cast.blade.php
+++ resources/views/cast.blade.php
@@ -3,10 +3,9 @@
 @section('title', 'The Cast &amp; Crew')
 
 @section('content')
-
+    <h1>The Cast &amp; Crew</h1>
 	<div class="row">
 		<div class="col-md-6 col-xs-12">
-		    <h1>The Cast &amp; Crew</h1>
 		    <p><i>The Cry of Jazz</i> wouldn't have been possible without every participant volunteering their time. Approximately 65 people&mdash;actors, musicians, crew&mdash;worked for free. Experience and exposure is what everyone was offered instead of a wage. The actors consisted of both students and local amateurs from Chicago's black community theater scene. The crew was made up of friends and aquaintances who assisted in everything from driving actors and crew around town to providing the apartment set and props used in the film.</p>
 		    <p>The musicians performing in the film in various guises are Le Sun Ra &amp; his Arkestra, featuring the likes of John Gilmour and Julian Priester. Paid a nominal fee, Sun Ra was looking to promote music from his own El Saturn Records label used in the film, as well as hoped it would garner future live gigs.</p>
 		    <p>Two cinematographers shot the film: Hank Starr and Howard Alk. Alk&mdash;who Ed Bland knew from the Gate of Horn music club where <i>The Cry</i>'s music scenes were shot&mdash;was also a film editor, and was hired to edit <i>The Cry</i>. He would later go on to make a number of acclaimed documentary films on Fred Hampton and Bob Dylan.</p>
