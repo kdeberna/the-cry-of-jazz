fca9f938e15cee86ffc6e8325b4df9fc4ec4e58d
Kevin DeBernardi <kevin@analoglifestyle.com>
2018-01-03 18:03:50 -0500

    Restyle index page for desktop as well as mobile

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,75 +3,89 @@
 @section('title', 'Home')
 
 @section('pre-content')
-    <div class="embed-container">
+    <div class="embed-container visible-xs-block">
         <iframe src="https://player.vimeo.com/video/243012636" frameborder="0"></iframe>
     </div>
 @endsection
 
 @section('content')
-	<div class="rotating-quotes">
-		<div class="quote">
-			<p>
-				Scarcely less abrasive now than when it first appeared.
-			</p>
-			<p class="author">
-				J. Hoberman, <i>Village Voice</i>
-			</p>
-		</div>
-		<div class="quote">
-			<p>
-				The Negro is the only human American.
-			</p>
-			<p class="author">
-					<i>The Cry of Jazz</i>
-			</p>
-		</div>
-		<div class="quote">
-			<p>
-				One of the most historically significant examples of African-American filmmaking.
-			</p>
-			<p class="author">
-				Seth Watter, WFMU's <i>Beware of the Blog</i>
-			</p>
-		</div>
-		<div class="quote">
-			<p>
-				Jazz was the Negro's act of transcendence.
-			</p>
-			<p class="author">
-				Edward Bland, <i>Film Culture</i>
-			</p>
-		</div>
-		<div class="quote">
-			<p>
-				Jazz is the one element of American life where Whites must be humble to Negroes.
-			</p>
-			<p class="author">
-				<i>The Cry of Jazz</i>
-			</p>
-		</div>
-		<div class="quote">
-			<p>
-				Bland's take on race and culture in America is still ahead of its time.
-			</p>
-			<p class="author">
-				Damon Lock, <i>Stop Smiling</i>
-			</p>
-		</div>
-		<div class="quote">
-			<p>
-				If White America can accept the Negro as the American hero, White America will have come of age.
-			</p>
-			<p class="author">
-				<i>The Cry of Jazz</i>
-			</p>
+	<div class="row hidden-xs">
+		<div class="col-md-12">
+		    <div class="embed-container" style="margin-top: 30px; margin-bottom: 50px;">
+		        <iframe src="https://player.vimeo.com/video/243012636" frameborder="0"></iframe>
+		    </div>
 		</div>
 	</div>
-    <div>
-    	<p><i>The Cry of Jazz</i> is the only film composer and musician Ed Bland ever helmed. It fused street grit and ivory tower intellect into a thirty-four minute celluloid whirlwind unlike anything before or since, all the while scored by a then unknown Sun Ra &amp; his Arkestra. Deemed radical, alarmist, and amateurish by many upon its initial release, this black and white gem was eventually considered worthy enough by the Library of Congress for preservation in the United States National Film Registry in 2010.</p>
-		<a href="{{ url('film') }}" class="btn btn-default" style="width: 100%; margin-top: 8px;">Read More</a>
-		<a href="{{ url('purchase') }}" class="btn btn-default" style="width: 100%; margin-top: 16px;">Purchase the Film</a>
-    </div>
+	<div class="row">
+		<div class="col-md-6 col-xs-12">
+			<div class="rotating-quotes">
+				<div class="quote">
+					<p>
+						Scarcely less abrasive now than when it first appeared.
+					</p>
+					<p class="author">
+						J. Hoberman, <i>Village Voice</i>
+					</p>
+				</div>
+				<div class="quote">
+					<p>
+						The Negro is the only human American.
+					</p>
+					<p class="author">
+							<i>The Cry of Jazz</i>
+					</p>
+				</div>
+				<div class="quote">
+					<p>
+						One of the most historically significant examples of African-American filmmaking.
+					</p>
+					<p class="author">
+						Seth Watter, WFMU's <i>Beware of the Blog</i>
+					</p>
+				</div>
+				<div class="quote">
+					<p>
+						Jazz was the Negro's act of transcendence.
+					</p>
+					<p class="author">
+						Edward Bland, <i>Film Culture</i>
+					</p>
+				</div>
+				<div class="quote">
+					<p>
+						Jazz is the one element of American life where Whites must be humble to Negroes.
+					</p>
+					<p class="author">
+						<i>The Cry of Jazz</i>
+					</p>
+				</div>
+				<div class="quote">
+					<p>
+						Bland's take on race and culture in America is still ahead of its time.
+					</p>
+					<p class="author">
+						Damon Lock, <i>Stop Smiling</i>
+					</p>
+				</div>
+				<div class="quote">
+					<p>
+						If White America can accept the Negro as the American hero, White America will have come of age.
+					</p>
+					<p class="author">
+						<i>The Cry of Jazz</i>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 col-xs-12">
+		    <div>
+		    	<p><i>The Cry of Jazz</i> is the only film composer and musician Ed Bland ever helmed. It fused street grit and ivory tower intellect into a thirty-four minute celluloid whirlwind unlike anything before or since, all the while scored by a then unknown Sun Ra &amp; his Arkestra. Deemed radical, alarmist, and amateurish by many upon its initial release, this black and white gem was eventually considered worthy enough by the Library of Congress for preservation in the United States National Film Registry in 2010.</p>
+				<a href="{{ url('film') }}" class="btn btn-default" style="width: 100%; margin-top: 8px;">Read More</a>
+				<a href="{{ url('purchase') }}" class="btn btn-default" style="width: 100%; margin-top: 16px;">Purchase the Film</a>
+		    </div>
+		</div>
+	</div>
+
 @endsection
 
 @section('scripts')
