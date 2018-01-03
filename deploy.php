97400aa09691df8bbfc8a189eb57d78557707b65
Kevin DeBernardi <kevin@analoglifestyle.com>
2018-01-03 18:02:25 -0500

    Add deploy script

---
+++ deploy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/common.php';
+require 'vendor/deployer/recipes/rsync.php';
+
+// Configuration
+
+set('default_stage', 'preview');
+set('ip', '159.89.43.220');
+set('shared_dirs', ['storage']);
+set('writable_dirs', get('shared_dirs'));
+
+set('rsync_src', __DIR__);
+set('rsync_dest', '/var/www/thecryofjazz.com/release');
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+set('keep_releases', 3);
+
+set('rsync',[
+    'exclude'      => [
+        '.git',
+        '.circleci',
+        '.DS_Store',
+        'tests',
+        '.env.example',
+        '.gitattributes',
+        '.gitignore',
+        'composer.json',
+        'composer.lock',
+        'deploy.php',
+        'package.json',
+        'phpunit.xml',
+        'server.php',
+        'webpack.mix.js',
+        'yarn.lock',
+    ],
+    'exclude-file' => false,
+    'include'      => [],
+    'include-file' => false,
+    'filter'       => [],
+    'filter-file'  => false,
+    'filter-perdir'=> false,
+    'flags'        => 'rzv',
+    'options'      => [],
+    'timeout'      => 0,
+]);
+
+// Servers
+
+server('digitalocean', get('ip'))
+    ->user('root')
+    ->identityFile()
+    ->stage('preview')
+    ->set('deploy_path', '/var/www/thecryofjazz.com')
+    ->set('http_user', 'www-data');
+
+// Tasks
+
+desc('Deploy The Cry of Jazz website');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'rsync',
+    // 'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:unlock',
+    'deploy:symlink',
+    'cleanup',
+    'success'
+]);
